1a782ced8c8024155e3cb8fc2d120c2360adac20
sun977 <1115476069@qq.com>
2025-12-04 15:19:37 +0800

    docs(router): 更新资产路由日期并新增编排器路由文件
    更新资产路由文件的日期注释，并新增扫描编排器路由模块的基础文件结构
    
    
    Former-commit-id: 439f1f4ee1b79b91a7f452e72152eab7b2ef0b8b

---
+++ neoMaster/internal/app/master/router/orchestrator_routers.go
@@ -0,0 +1,13 @@
+/**
+ * 路由:扫描编排器路由
+ * @author: sun977
+ * @date: 2025.12.04
+ * @description: 扫描编排器路由模块
+ * @func: 未完成
+ */
+package router
+
+import "github.com/gin-gonic/gin"
+
+func (r *Router) setupOrchestratorRoutes(v1 *gin.RouterGroup) {
+}

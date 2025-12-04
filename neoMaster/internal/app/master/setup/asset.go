cdbe456ecf9724efe8b968b97a51c62c5c6d8888
sun977 <1115476069@qq.com>
2025-12-04 16:27:45 +0800

    feat(asset): 实现资产管理模块的初始化和路由功能
    - 新增资产管理模块的初始化逻辑，包括Repository、Service和Handler的装配
    - 添加资产管理相关路由，支持主机的CRUD操作和服务管理
    - 完善资产相关的API响应格式和错误处理
    - 移除不再使用的编排器模块代码

---
+++ neoMaster/internal/app/master/setup/asset.go
@@ -0,0 +1,46 @@
+/**
+ * 初始化:资产管理模块
+ * @author: sun977
+ * @date: 2025.12.04
+ * @description: 资产管理模块初始化
+ */
+package setup
+
+import (
+	"neomaster/internal/pkg/logger"
+
+	assetHandler "neomaster/internal/handler/asset"
+	assetRepo "neomaster/internal/repo/mysql/asset"
+	assetService "neomaster/internal/service/asset"
+
+	"gorm.io/gorm"
+)
+
+// BuildAssetModule 构建资产管理模块
+func BuildAssetModule(db *gorm.DB) *AssetModule {
+	logger.WithFields(map[string]interface{}{
+		"path":      "setup.asset",
+		"operation": "build_module",
+		"func_name": "setup.BuildAssetModule",
+	}).Info("开始初始化资产管理模块")
+
+	// 1. Repository 初始化
+	hostRepo := assetRepo.NewAssetHostRepository(db)
+
+	// 2. Service 初始化
+	hostService := assetService.NewAssetHostService(hostRepo)
+
+	// 3. Handler 初始化
+	hostHandler := assetHandler.NewAssetHostHandler(hostService)
+
+	logger.WithFields(map[string]interface{}{
+		"path":      "setup.asset",
+		"operation": "build_module",
+		"func_name": "setup.BuildAssetModule",
+	}).Info("资产管理模块初始化完成")
+
+	return &AssetModule{
+		AssetHostHandler: hostHandler,
+		AssetHostService: hostService,
+	}
+}

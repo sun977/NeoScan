d81bebdd3db21aa7b3568192173a04f693537459
sun977 <1115476069@qq.com>
2026-01-06 18:47:34 +0800

    feat(etl): 添加资产合并功能并集成ETL处理器
    - 在orchestrator模块中初始化AssetMerger用于合并主机和web资产
    - 将ETL处理器集成到router和app中，支持启动和停止
    - 更新调度引擎相关方法以同时管理ETL处理器
    
    
    Former-commit-id: bc93ef8ee5ed29c7dbe59b02669069a750ee6388

--- neoMaster/internal/app/master/setup/orchestrator.go
+++ neoMaster/internal/app/master/setup/orchestrator.go
@@ -86,7 +86,13 @@ func BuildOrchestratorModule(db *gorm.DB, cfg *config.Config, tagService tag_sys
 	if etlWorkerNum <= 0 {
 		etlWorkerNum = 5 // 默认值
 	}
-	etlProcessor := etl.NewResultProcessor(resultQueue, etlWorkerNum)
+
+	// 初始化 AssetMerger
+	hostRepo := assetRepo.NewAssetHostRepository(db)
+	webRepo := assetRepo.NewAssetWebRepository(db)
+	assetMerger := etl.NewAssetMerger(hostRepo, webRepo)
+
+	etlProcessor := etl.NewResultProcessor(resultQueue, assetMerger, etlWorkerNum)
 	// TODO: 在应用启动时调用 etlProcessor.Start(ctx)
 
 	// 3. Service 初始化
